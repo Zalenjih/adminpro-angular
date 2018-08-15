19c4415eb50281e01b653b11fc1bc7c9513e1ed3
Humberto Zapata León <33710098+Zalenjih@users.noreply.github.com>
2018-08-15 16:41:06 -0500

    Creación de rutas hijas forChild()

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,16 +8,16 @@ import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 const routes: Routes = [
-    {
+    /* {
         path: '',
         component: PagesComponent,
         children: [
             { path: 'dashboard', component: DashboardComponent },
             { path: 'progress', component: ProgressComponent },
             { path: 'grafica1', component: Graficas1Component },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
         ]
-    },
+    }, */
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent },
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,6 +7,7 @@ import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+import { PAGES_ROUTES } from './pages.routes';
 
 @NgModule({
     declarations: [
@@ -21,7 +22,8 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         Graficas1Component
     ],
     imports: [
-        SharedModule
+        SharedModule,
+        PAGES_ROUTES
     ]
 })
 
